5ce163a9c9d69cdf1061a60efc387e6870af9526
ckoopmann <c.k.e.koopmann@gmail.com>
2023-10-11 16:10:29 +0800

    Implement signature verification

--- contracts/contracts/Flash.sol
+++ contracts/contracts/Flash.sol
@@ -10,11 +10,19 @@ import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC2
 import {
     SafeERC20
 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 
-contract Flash is GelatoRelayContext {
+contract Flash is GelatoRelayContext, EIP712 {
 
     using SafeERC20 for IERC20;
+
+    bytes32 private constant _PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+    bytes32 public constant PAY_TYPEHASH =
+        keccak256("Pay(address receiver,bytes32 permitHash)");
 
     struct Signature {
         uint8 v;
@@ -34,10 +42,15 @@ contract Flash is GelatoRelayContext {
         PermitData permitData;
     }
 
+    constructor() EIP712("Flash", "1") {}
+
     function pay(
         PaymentData calldata _paymentData,
         Signature calldata _signature
     ) external onlyGelatoRelay {
+
+        _verifySignature(_paymentData, _signature);
+
         IERC20Permit(_paymentData.token).permit(
             _paymentData.from,
             address(this),
@@ -63,4 +76,29 @@ contract Flash is GelatoRelayContext {
         );
 
     }
+
+    function _verifySignature(
+        PaymentData calldata _paymentData,
+        Signature calldata _signature
+    ) internal {
+        uint256 userNonce = IERC20Permit(_paymentData.token).nonces(_paymentData.from);
+        bytes32 permitStructHash = keccak256(abi.encode(
+            _PERMIT_TYPEHASH,
+            _paymentData.from,
+            address(this),
+            _paymentData.amount,
+            userNonce,
+            _paymentData.permitData.deadline
+        ));
+
+        bytes32 paymentStructHash = keccak256(abi.encode(
+            PAY_TYPEHASH,
+            _paymentData.to,
+            permitStructHash
+        ));
+        bytes32 hash = _hashTypedDataV4(paymentStructHash);
+        address signer = ECDSA.recover(hash, _signature.v, _signature.r, _signature.s);
+        require(signer == _paymentData.from, "Flash: invalid signature");
+    }
+
 }

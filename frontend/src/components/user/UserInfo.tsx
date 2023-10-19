f1823e6eb464ce4a942d5c154e3231f29aca2bb8
ckoopmann <c.k.e.koopmann@gmail.com>
2023-10-19 11:50:17 +0800

    First version of payment history table

--- frontend/src/components/user/UserInfo.tsx
+++ frontend/src/components/user/UserInfo.tsx
@@ -1,17 +1,30 @@
 "use client";
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useWeb3Auth } from "@/provider/Web3AuthProvider";
-import { Label } from "@/components/ui/label"
-import { getGnosisSdk } from '@dethcrypto/eth-sdk-client'
+import { Label } from "@/components/ui/label";
+import { getGnosisSdk } from "@dethcrypto/eth-sdk-client";
 import { ethers } from "ethers";
-import { PaymentsReceivedDocument,  PaymentsSentDocument, execute } from '../../../.graphclient'
+import {
+    PaymentsReceivedDocument,
+    PaymentsSentDocument,
+    execute,
+} from "../../../.graphclient";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
 
 type Payment = {
     direction: "sent" | "received";
     amount: string;
     transactionHash: string;
+    timestamp: number;
 };
-function UserInfo({className}: {className?: string}) {
+function UserInfo({ className }: { className?: string }) {
     const web3AuthContext = useWeb3Auth();
     const [userAddress, setUserAddress] = useState("...");
     const [userBalance, setUserBalance] = useState("...");
@@ -19,7 +32,7 @@ function UserInfo({className}: {className?: string}) {
     useEffect(() => {
         const init = async () => {
             const signer = web3AuthContext?.ethersSigner;
-            if(!signer){
+            if (!signer) {
                 console.error("Getting user signer failed");
                 return;
             }
@@ -39,20 +52,33 @@ function UserInfo({className}: {className?: string}) {
             }
             console.log("User Balance: ", balance);
             setUserBalance(ethers.utils.formatUnits(balance, 6));
-            const paymentsReceived  = await execute(PaymentsReceivedDocument, {user: address});
+            const paymentsReceived = await execute(PaymentsReceivedDocument, {
+                user: address,
+            });
             console.log("Payments received: ", paymentsReceived);
-            let payments = paymentsReceived.data.payments.map((payment: any) => ({
-                direction: "received",
-                amount: ethers.utils.formatUnits(payment.amount, 6),
-                transactionHash: payment.transactionHash,
-            }));
-            const paymentsSent = await execute(PaymentsSentDocument, {user: address});
+            let payments = paymentsReceived.data.payments.map(
+                (payment: any) => ({
+                    direction: "received",
+                    amount: ethers.utils.formatUnits(payment.amount, 6),
+                    timestamp: parseInt(payment.blockTimestamp),
+                    transactionHash: payment.transactionHash,
+                })
+            );
+            const paymentsSent = await execute(PaymentsSentDocument, {
+                user: address,
+            });
             console.log("Payments sent: ", paymentsSent);
-            payments = payments.concat(paymentsSent.data.payments.map((payment: any) => ({
-                direction: "sent",
-                amount: ethers.utils.formatUnits(payment.amount, 6),
-                transactionHash: payment.transactionHash,
-            })));
+            payments = payments.concat(
+                paymentsSent.data.payments.map((payment: any) => ({
+                    direction: "sent",
+                    amount: ethers.utils.formatUnits(payment.amount, 6),
+                    timestamp: parseInt(payment.blockTimestamp),
+                    transactionHash: payment.transactionHash,
+                }))
+            );
+            payments.sort((a: any, b: any) =>
+                a.timestamp > b.timestamp ? -1 : 1
+            );
             setPayments(payments);
         };
         init();
@@ -62,11 +88,29 @@ function UserInfo({className}: {className?: string}) {
         <>
             <div className={className}>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <Label className="text-xl"><a href={`https://gnosisscan.io/token/0xddafbb505ad214d7b80b1f830fccc89b60fb7a83?a=${userAddress}`}>Current Balance:</a></Label>
+                    <Label className="text-xl">Current Balance:</Label>
                     <div className="text-3xl font-bold">{userBalance} $</div>
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <pre>{JSON.stringify(payments, null, 2)}</pre>
+                    <Label className="text-xl">Transaction History:</Label>
+                    <Table>
+                        <TableBody>
+                            {payments.map((payment) => (
+                                <TableRow>
+                                    <TableCell>
+                                        {payment.timestamp
+                                            ? new Date(
+                                                  payment.timestamp * 1000
+                                              ).toLocaleString()
+                                            : ""}
+                                    </TableCell>
+                                    <TableCell className="text-right">
+                                        {payment.direction == "received" ? "+" : "-"} {payment.amount} $
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
                 </div>
             </div>
         </>

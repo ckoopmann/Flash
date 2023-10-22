5a5e498dbb8679f7a79769f7199fd7f0d03d72fa
ckoopmann <c.k.e.koopmann@gmail.com>
2023-10-22 11:18:06 +0800

    Add Flash Logo

--- frontend/src/app/dapp/layout.tsx
+++ frontend/src/app/dapp/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Navigationbar from "@/components/Navigationbar";
 import {Web3AuthProvider} from "@/provider/Web3AuthProvider";
+import { BsLightning } from "react-icons/bs";
 
 export default function RootLayout({
     children,
@@ -9,8 +10,8 @@ export default function RootLayout({
 }) {
         return (
             <main>
-                <div className="flex justify-center items-center fixed top-0 bg-black text-white w-screen h-16 z-50">
-                    <div className="text-3xl">Flash</div>
+                <div className="flex flex-col justify-center items-center fixed top-0 bg-black text-white w-screen h-16 z-50">
+                    <BsLightning size={42} />
                 </div>
                 <Web3AuthProvider>
                     {children}
